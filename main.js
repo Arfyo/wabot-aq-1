b32e1250717dea669df6f74dc962598e0cebee9a
Arfyo <92430978+Arfyo@users.noreply.github.com>
2021-12-07 20:03:44 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 require('./config.js')
-const { WAConnection: _WAConnection } = require('^3.5.3')
+const { WAConnection: _WAConnection } = require('@adiwajshing/baileys')
 const cloudDBAdapter = require('./lib/cloudDBAdapter')
 const { generate } = require('qrcode-terminal')
 const syntaxerror = require('syntax-error')
